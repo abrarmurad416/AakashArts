22d26876a38d33787dd62a130045fd9201c85b9d
Abrar <abrarmurad00@gmail.com>
2024-02-28 22:58:44 -0500

    initial navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
+import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
+      <Navbar />
       Aakash Arts
     </main>
   );
