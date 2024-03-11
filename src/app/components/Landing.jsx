b409b3887e48a97ec3a33c1901d90a4fd3974bbf
Abrar <abrarmurad00@gmail.com>
2024-03-10 23:46:02 -0400

    description changes

--- src/app/components/Landing.jsx
+++ src/app/components/Landing.jsx
@@ -23,7 +23,7 @@ export default function Landing() {
                 <div class="content">
                   <h4>Textile Printing </h4>
                   <p>
-                  At Aakash Arts, we excel in the art of textile printing, utilizing cutting-edge techniques to bring your designs to life on a variety of fabrics. Whether you're looking to add intricate patterns to garments or create eye-catching textiles for home decor, our skilled team ensures high-quality prints that capture the essence of your vision. From digital printing to screen printing and everything in between, we deliver exceptional results that elevate your textiles to new heights.
+                  At Aakash Arts, we excel in the art of textile printing, utilizing cutting-edge techniques to bring your designs to life on a variety of fabrics. Whether you are looking to add intricate patterns to garments or create eye-catching textiles for home decor, our skilled team ensures high-quality prints that capture the essence of your vision. From digital printing to screen printing and everything in between, we deliver exceptional results that elevate your textiles to new heights.
                   </p>
                   <p>
                     <a href="#">Learn more</a>
@@ -41,7 +41,7 @@ export default function Landing() {
                 <div class="content">
                   <h4>Custom Apparel Design</h4>
                   <p>
-                  Elevate your wardrobe with custom apparel designed by Aakash Arts. Our talented designers work closely with clients to create personalized clothing that reflects their individual style and personality. Whether it's for a special event, promotional merchandise, or everyday wear, we offer a wide range of customization options, including embroidery, vinyl printing, and dye-sublimation. With meticulous attention to detail and a commitment to quality, we bring your apparel ideas to life with unparalleled craftsmanship.
+                  Elevate your wardrobe with custom apparel designed by Aakash Arts. Our talented designers work closely with clients to create personalized clothing that reflects their individual style and personality. Whether it is for a special event, promotional merchandise, or everyday wear, we offer a wide range of customization options, including embroidery, vinyl printing, and dye-sublimation. With meticulous attention to detail and a commitment to quality, we bring your apparel ideas to life with unparalleled craftsmanship.
                   </p>
                   <p>
                     <a href="#">Learn more</a>
@@ -59,7 +59,8 @@ export default function Landing() {
                 <div class="content">
                   <h4> Art Commissions </h4>
                   <p>
-                  Unlock the potential of your imagination with art commissions from Aakash Arts. Our team of skilled artists specializes in bringing your visions to life on canvas, paper, or any medium of your choosing. Whether you're seeking a stunning portrait, a captivating mural, or a custom illustration, we work closely with you to understand your concept and bring it to fruition with precision and creativity. With a dedication to excellence and a passion for artistic expression, we transform your ideas into timeless masterpieces that inspire and enchant.
+                  Unlock the potential of your imagination with art commissions from Aakash Arts. Our team of skilled artists specializes in bringing your visions to life on canvas, paper, or any medium of your choosing. Whether you are seeking a stunning portrait, a captivating mural, or a custom illustration, we work closely with you to understand your concept and bring it to fruition with precision and creativity. With a dedication to excellence and a passion for artistic expression, we transform your ideas into timeless masterpieces that inspire and enchant.
+
                   </p>
                   <p>
                     <a href="#">Learn more</a>
